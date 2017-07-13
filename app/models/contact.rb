7469c4461b2b247e3e9cbb31ecf860a7e9c224cb
Bona Lee <bonalee3@gmail.com>
2017-07-12 20:18:05 -0500

    Added a +81 country code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+  def friendly_time_created
+    created_at.strftime("%e %b %Y %H:%M:%S%p")
+  end
+
+  def full_name
+    return first_name + " " + last_name
+  end
+
+  def japan_code
+    prefix = "+81"
+    return prefix + phone_number
+  end
 end
